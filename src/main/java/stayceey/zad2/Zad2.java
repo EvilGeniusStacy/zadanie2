13eaffb34842b968637a031efad7cc9b6af73a2e
Robert Matuszewski <robert.matuszewski556@gmail.com>
2024-10-26 15:10:10 +0200

    newcomit

--- src/main/java/stayceey/zad2/Zad2.java
+++ src/main/java/stayceey/zad2/Zad2.java
@@ -4,12 +4,9 @@ public class Zad2 {
     
     public static void main(String[] args) {
         Scanner pierwszy =new Scanner(System.in);
-        Scanner drugi = new Scanner(System.in);
-        System.out.println("Podaj liczbę całkowitą.");
-        int a = pierwszy.nextInt();
-        System.out.println("Podaj drugą liczbę całkowitą.");
-        int b = drugi.nextInt();
-        int c = a+b;
-        System.out.println("Suma twoich liczb to "+c);
+        System.out.println("Napisz zdanie które ma być zapisane capslockiem.");
+        String tekst = pierwszy.nextLine();
+        String caps =tekst.toUpperCase();
+        System.out.println(caps);
     }
 }
